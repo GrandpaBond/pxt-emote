be8a3f67ecec5ecb4856000d4e2290add5b17f31
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-04-22 18:06:53 +0100

    Added bound-constraining for eyes & mouths array access

--- main.ts
+++ main.ts
@@ -19,6 +19,7 @@ enum EYES {
     //% block="Flip"
     FLIP
 }
+const N_EYES = 9
 
 enum MOUTHS {
     //% block="Flat"
@@ -46,6 +47,7 @@ enum MOUTHS {
     //% block="Flip"
     FLIP
 }
+const N_MOUTHS = 12
 
 enum MOODS {
     //% block="None"
@@ -69,10 +71,12 @@ enum MOODS {
     //% block="Dead"
     DEAD
 }
+const N_MOODS = 10
+
 //% color="#4080e0" icon="\uf118"
 namespace Emote {
 
-// ensure these constants match the enum ordering!
+    // ensure these constants match the enum ordering!
     const all_eyes = [
         891,  //"Open"
         874,  //"Sad"
@@ -113,20 +117,19 @@ namespace Emote {
         build(eyes: EYES, mouth: MOUTHS) {
             this.eyes = eyes
             this.mouth = mouth
-            this.pixels = all_mouths[this.mouth] * 1024 
-                        + all_eyes[this.eyes]
+            this.pixels = all_mouths[this.mouth] << 10 + all_eyes[this.eyes]
         }
         show() {
-            let pixels = this.pixels
+            let bitmap:number = this.pixels
             let x = 0
             let y = 0
-            while (pixels) {
-                if (pixels & 1) {
+            while (bitmap) {
+                if (bitmap & 1) {
                     led.plot(x, y)
                 } else {
                     led.unplot(x, y)
                 }
-                pixels >>= 1
+                bitmap >>= 1
                 x++
                 if (x > 4) {
                     y++
@@ -136,7 +139,6 @@ namespace Emote {
         }
     }
 
-
     const Face1 = new Face(0, 0);
     const Face2 = new Face(0, 0);
 
@@ -144,67 +146,71 @@ namespace Emote {
     let switch_gap = 0;
     let switch_time = 0
     let switch_vary = 0
-    let my_mood: MOODS = -1
 
     //% block="Show face with eyes= $eyes, mouth= $mouth"
     export function emote(eyes: EYES, mouth: MOUTHS) {
         switching = false
-        Face1.build(eyes, mouth)
+        // constrain to currently defined values
+        let e = (eyes > -1) ? eyes : 0
+        e = (e < N_EYES) ? e : N_EYES - 1
+        let m = (mouth > -1) ? mouth : 0
+        m = (mouth < N_MOUTHS) ? mouth : N_MOUTHS - 1
+        Face1.build(e, m)
         Face1.show()
     }
 
     //% block="React as $mood"
     export function new_mood(mood: MOODS) {
-        if (my_mood != mood) {
-            my_mood = mood
-            if (my_mood == MOODS.SNORING) {
-                set_mood(EYES.SHUT, MOUTHS.FLAT, EYES.SHUT, MOUTHS.OPEN, 2000, 2000, 0)
-            } else if (my_mood == MOODS.ASLEEP) {
-                set_mood(EYES.SHUT, MOUTHS.FLAT, EYES.SHUT, MOUTHS.HMMM, 3000, 500, 0)
-            } else if (my_mood == MOODS.NONE) {
-                set_mood(EYES.OPEN, MOUTHS.FLAT, EYES.SHUT, MOUTHS.FLAT, 600, 300, 2)
-            } else if (my_mood == MOODS.HAPPY) {
-                set_mood(EYES.OPEN, MOUTHS.GRIN, EYES.UP, MOUTHS.KISS, 1500, 400, 2)
-            } else if (my_mood == MOODS.SAD) {
-                set_mood(EYES.SAD, MOUTHS.SULK, EYES.SHUT, MOUTHS.SULK, 4000, 600, 1)
-            } else if (my_mood == MOODS.ANGRY) {
-                set_mood(EYES.MAD, MOUTHS.HMMM, EYES.MAD, MOUTHS.SHOUT, 2000, 800, 1)
-            } else if (my_mood == MOODS.SURPRISED) {
-                set_mood(EYES.POP, MOUTHS.OPEN, EYES.OPEN, MOUTHS.OPEN, 1600, 400, 0)
-            } else if (my_mood == MOODS.SHIVER) {
-                set_mood(EYES.LEFT, MOUTHS.RIGHT, EYES.RIGHT, MOUTHS.LEFT, 250, 250, 0)
-            } else if (my_mood == MOODS.TICKLE) {
-                set_mood(EYES.OPEN, MOUTHS.OK, EYES.FLIP, MOUTHS.FLIP, 750, 250, 0)
-            }
-            if (my_mood == MOODS.DEAD) {
-                basic.showIcon(IconNames.Skull)
-            } else {
-                Face1.show()
-            }
+        // constrain to currently defined values
+        let m = (mood > -1) ? mood : 0
+        m = (mood < N_MOODS) ? m : N_MOODS - 1
+        if (m == MOODS.SNORING) {
+            set_mood(EYES.SHUT, MOUTHS.FLAT, EYES.SHUT, MOUTHS.OPEN, 2000, 2000, 0)
+        } else if (m == MOODS.ASLEEP) {
+            set_mood(EYES.SHUT, MOUTHS.FLAT, EYES.SHUT, MOUTHS.HMMM, 3000, 500, 0)
+        } else if (m == MOODS.NONE) {
+            set_mood(EYES.OPEN, MOUTHS.FLAT, EYES.SHUT, MOUTHS.FLAT, 600, 300, 2)
+        } else if (m == MOODS.HAPPY) {
+            set_mood(EYES.OPEN, MOUTHS.GRIN, EYES.UP, MOUTHS.KISS, 1500, 400, 2)
+        } else if (m == MOODS.SAD) {
+            set_mood(EYES.SAD, MOUTHS.SULK, EYES.SHUT, MOUTHS.SULK, 4000, 600, 1)
+        } else if (m == MOODS.ANGRY) {
+            set_mood(EYES.MAD, MOUTHS.HMMM, EYES.MAD, MOUTHS.SHOUT, 2000, 800, 1)
+        } else if (m == MOODS.SURPRISED) {
+            set_mood(EYES.POP, MOUTHS.OPEN, EYES.OPEN, MOUTHS.OPEN, 1600, 400, 0)
+        } else if (m == MOODS.SHIVER) {
+            set_mood(EYES.LEFT, MOUTHS.RIGHT, EYES.RIGHT, MOUTHS.LEFT, 250, 250, 0)
+        } else if (m == MOODS.TICKLE) {
+            set_mood(EYES.OPEN, MOUTHS.OK, EYES.FLIP, MOUTHS.FLIP, 750, 250, 0)
         }
+        if (m == MOODS.DEAD) {
+            basic.showIcon(IconNames.Skull)
+        } else {
+            Face1.show()
+        }
+
     }
 
     function set_mood(eyes: EYES, mouth: MOUTHS, other_eyes: EYES, other_mouth: MOUTHS, gap: number, time: number, vary: number) {
         switching = false
         Face1.build(eyes, mouth)
         Face2.build(other_eyes, other_mouth)
-        if (gap > 0) {
-            // In most moods, we temporarily switch between two faces.
-            // So we may be blinking, snoring, shivering or laughing etc.
-            // These are controlled by two time-periods: switch_gap sets how often, 
-            // and switch_time says how long to show the alternate face.
-            switch_gap = gap
-            switch_time = time
-            switch_vary = vary
-            switching = true
-            control.inBackground(function () {
-                while (switching) {
-                    pause(randint(switch_gap, switch_vary * switch_gap))
-                    Face2.show()
-                    pause(switch_time)
-                    Face1.show()
-                }
-            })
-        }
+        // In most moods, we temporarily switch between two faces.
+        // So we may be blinking, snoring, shivering or laughing etc.
+        // These are controlled by two time-periods: "switch_gap" governs how often, 
+        // and "switch_time" says how long to show the alternate face.
+        // A non-zero "switch_vary" allows for an unpredictable multiple of gap times. 
+        switch_gap = gap
+        switch_time = time
+        switch_vary = vary
+        switching = true
+        control.inBackground(function () {
+            while (switching) {
+                pause(switch_gap + randint(0, switch_vary) * switch_gap)
+                Face2.show()
+                pause(switch_time)
+                Face1.show()
+            }
+        })
     }
 }
